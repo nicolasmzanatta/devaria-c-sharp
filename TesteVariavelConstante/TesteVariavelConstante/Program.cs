b0019a7e6841d7dce106cd68330de855573c7e6c
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-04-26 21:28:26 -0300

    meu primeiro commit c#

---
+++ TesteVariavelConstante/TesteVariavelConstante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TesteVariavelConstante
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+        {
+            const string constante = "uma constante que não muda";
+
+            if(argumentos.Length == 0)
+            {
+                Console.WriteLine("Favor informar um número inteiro");
+                return; //ela para a execuçao se for atingido 
+            }
+
+            int inteiro;
+            bool teste = int.TryParse(argumentos[0], out inteiro); // tryparse tenta converter algo em inteiro 
+
+            if(teste == false)
+            {
+                Console.WriteLine("O argumento informado não é um número inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+        }
+    }
+}
