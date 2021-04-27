c8dc9ee681831005e978f60231423b36fba7504e
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-04-27 00:50:03 -0300

    parte 3 e 4 da aula 1

---
+++ ListaDeConvidados/ListaDeConvidados/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ListaDeConvidados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do convidado:");
+            var nome = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(nome) || string.IsNullOrWhiteSpace(nome))
+            {
+                Console.WriteLine("Nome não informado para seguir com o programa");
+                return; //serve para parar o programa por aqui, pq se a pessoa nao informou o nome, nao tem pq seguir...
+            }
+
+            Console.WriteLine("Informe a idade do convidado:");
+            var idadeString = Console.ReadLine();
+            int idade;
+
+            bool idadeInformada = int.TryParse(idadeString, out idade);
+
+            if(idadeInformada == false)
+            {
+                Console.WriteLine("Idade não informada para seguir com o programa");
+                return;
+            }
+
+            bool estaConvidado;
+            switch(nome)
+            {
+                case "Daniel":
+                    estaConvidado = true;
+                    break;
+                case "Evandro":
+                    estaConvidado = true;
+                    break;
+                case "Matheus":
+                    estaConvidado = true;
+                    break;
+                case "Ale":
+                    estaConvidado = true;
+                    break;
+                case "Teresa":
+                    estaConvidado = true;
+                    break;
+                case "Gabriel":
+                    estaConvidado = true;
+                    break;
+                case "Cledson":
+                    estaConvidado = true;
+                    break;
+                default:
+                    estaConvidado = false;
+                    break;
+
+            }
+
+            if(estaConvidado == true && idade > 18)
+            {
+                Console.WriteLine("Parabéns, seja bem vindo a festa");
+            }
+            else if(estaConvidado == false)
+            {
+                Console.WriteLine("Você não está na lista de convidados, favor verificar com a pessoa que te convidou");
+            }
+            else
+            {
+                Console.WriteLine("Você não tem 18 anos para entrar na festa");
+            }
+        }
+    }
+}
